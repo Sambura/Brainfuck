6a907dd78e176aeba4302402bdc26a8d25f9fa24
Kirill Samburskiy <superkirs2@gmail.com>
2026-03-16 21:36:59 +0300

    [utils/interpreter] Implement interactive interpreter mode
    - Both interpreter implementations now support interactive mode: get input interactively from user and print output to console
    - Added a proper main function for interpreter.py
    - Added support for loading programs from a file
    - Added state dump opcode support in preprocessed interpreter implementation for debug purposes
    - Added an interactive version of sumN16 brainfuck program

--- programs/utils/interpreter.py
+++ programs/utils/interpreter.py
@@ -1,13 +1,14 @@
 from time import perf_counter
+from collections.abc import Iterable, Callable
+from itertools import takewhile
 
-def _bf_execute_basic(program: str, in_data: list[int], iter_limit:int=50000000) -> list[int]:
+def _bf_execute_basic(program: str, input_generator: Iterable[int], output_consumer: Callable[[int], None],
+                      iter_limit:int=50000000) -> None:
     """ Execute brainfuck program.
         Simplest interpreter: works directly on a string with the program   """
     memory = [0] * 32768
     pc = 0
     ptr = 0
-    iptr = 0
-    stdout = []
     loop_stack = []
     loop_iters = 0 # count only loop iterations to save on performance
 
@@ -22,15 +23,12 @@ def _bf_execute_basic(program: str, in_data: list[int], iter_limit:int=50000000)
             ptr += 1
         elif c == '<':
             ptr -= 1
-            assert ptr >= 0
+            if ptr < 0:
+                raise Exception('Memory underflow')
         elif c == '.':
-            stdout.append(memory[ptr])
+            output_consumer(memory[ptr])
         elif c == ',':
-            if iptr < len(in_data):
-                memory[ptr] = in_data[iptr] & 0xFF
-                iptr += 1
-            else:
-                memory[ptr] = 0
+            memory[ptr] = next(input_generator) & 0xFF
         elif c == '[':
             if memory[ptr] != 0:
                 loop_stack.append(pc)
@@ -51,17 +49,16 @@ def _bf_execute_basic(program: str, in_data: list[int], iter_limit:int=50000000)
                 raise Exception('Iteration limit reached')
 
         pc += 1
-    
-    return stdout
 
-# TODO: replace in_data with input_generator and do something similar with output
-def _bf_execute_preprocessed(program: str, in_data: list[int], iter_limit:int=100000000) -> list[int]:
+def _bf_execute_preprocessed(program: str, input_generator: Iterable[int], output_consumer: Callable[[int], None],
+                             iter_limit: int=100000000, state_dump_char=None) -> None:
     """ Execute brainfuck program.
         Processes program into list of opcodes before executing.
         Also combines consecutive +/- and </> instructions          """
     def preprocess(program: str):
-        opcodes = [0] # 0 - NOOP, 1 - ADD, 2 - SHIFT, 3 - BEGIN_LOOP, 4 - END_LOOP, 5 - PRINT, 6 - GET
+        opcodes = [0] # 0 - NOOP, 1 - ADD, 2 - SHIFT, 3 - BEGIN_LOOP, 4 - END_LOOP, 5 - PRINT, 6 - GET, 7 - CONTROL (custom command)
         consts = [0]
+        control_dict = {}
         loop_stack = []
 
         for i, c in enumerate(program):
@@ -98,18 +95,23 @@ def _bf_execute_preprocessed(program: str, in_data: list[int], iter_limit:int=10
                 opcodes.append(4)
                 consts.append(loop_start - 1) # PC for BEGIN_LOOP
                 consts[loop_start] = past_loop_end - 1 # PC for END_LOOP
+            elif c == state_dump_char:
+                message_indices = list(takewhile(lambda i: program[i] not in '+-[],.<>\n\r$', range(i + 1, len(program))))
+                message_end = message_indices[-1] if len(message_indices) > 0 else i
+
+                control_dict[len(opcodes) - 1] = program[i:message_end + 1]
+                opcodes.append(7)
+                consts.append(0)
         
         if len(loop_stack) > 0:
             raise Exception(f'Brainfuck syntax error: unmatched "["')
 
-        return opcodes[1:], consts[1:]
+        return opcodes[1:], consts[1:], control_dict
 
-    opcodes, consts = preprocess(program)
+    opcodes, consts, control_dict = preprocess(program)
     memory = [0] * 32768
     pc = 0
     ptr = 0
-    iptr = 0
-    stdout = []
     loop_iters = 0
 
     while pc < len(opcodes):
@@ -119,7 +121,8 @@ def _bf_execute_preprocessed(program: str, in_data: list[int], iter_limit:int=10
             memory[ptr] = (memory[ptr] + consts[pc]) & 0xFF
         elif opcode == 2: # SHIFT
             ptr += consts[pc]
-            assert ptr >= 0
+            if ptr < 0:
+                raise Exception('Memory underflow')
         elif opcode == 4 and memory[ptr] != 0: # END_LOOP
             pc = consts[pc]
             loop_iters += 1
@@ -128,36 +131,115 @@ def _bf_execute_preprocessed(program: str, in_data: list[int], iter_limit:int=10
         elif opcode == 3 and memory[ptr] == 0: # BEGIN_LOOP
             pc = consts[pc]
         elif opcode == 5:   # PRINT
-            stdout.append(memory[ptr])
+            output_consumer(memory[ptr])
         elif opcode == 6:   # GET
-            if iptr < len(in_data):
-                memory[ptr] = in_data[iptr] & 0xFF
-                iptr += 1
-            else:
-                memory[ptr] = 0
+            memory[ptr] = next(input_generator) & 0xFF
+        elif opcode == 7:   # CONTROL
+            if consts[pc] == 0: # CONTROL:0 state dump
+                print(f'\n[STATE DUMP]: {control_dict[pc]}. Program counter: {pc}, memory pointer: {ptr}. Memory:\n')
+                last_nonzero_index = [i for i, m in enumerate(memory) if m != 0][-1]
+                slice_start = 0
+                slice_width = 20
+                while slice_start - 1 < last_nonzero_index:
+                    current_start = slice_start
+                    memory_slice = memory[slice_start:slice_start + slice_width]
+                    slice_start += slice_width
+                    string_slice = ' '.join(f'{m: 3}' for m in memory_slice)
+                    print(f'[{current_start: 5}:{slice_start - 1: 5}]: {string_slice}')
 
         pc += 1
 
-    return stdout
-
-def bf_execute(program: str, in_data: list[int]=[], implementation='best') -> list[int]:
-    "Execute a brainfuck program with a given input. Returns data printed to stdout by the program"
+def _bf_get_interpreter(implementation: str='best'):
+    implementation = implementation.lower()
     if implementation == 'best' or implementation == 'preprocessed':
-        return _bf_execute_preprocessed(program, in_data)
+        return _bf_execute_preprocessed
     elif implementation == 'basic':
-        return _bf_execute_basic(program, in_data)
+        return _bf_execute_basic
     else:
         raise Exception(f'Invalid implementation name: "{implementation}"')
 
-if __name__ == '__main__':
-    program = input("Input a brainfuck program: ")
+def _bf_execute(program: str, in_data: list[int], interpreter: Callable) -> list[int]:
+    stdout = []
+
+    def input_generator():
+        yield from in_data
+        while True:
+            yield 0
+
+    interpreter(program, input_generator(), stdout.append)
+
+    return stdout
+
+def _bf_interactive_buffered_execute(program: str, interpreter: Callable, input_prompt: bool=False) -> float:
+    io_time = 0
+    last_char = None
+
+    def input_generator():
+        nonlocal io_time, last_char
+
+        while True:
+            start_time = perf_counter()
+            prompt = ('$> ' if last_char == '\n' else '\n$> ') if input_prompt else ''
+            last_char = '\n'
+            buffer = list(map(ord, input(prompt).encode().decode('unicode_escape'))) + [ord('\n')]
+            io_time += perf_counter() - start_time
+            yield from buffer
+
+    def output_consumer(char: int):
+        nonlocal last_char
+        last_char = char
+        print(chr(char), end='')
+
+    interpreter(program, input_generator(), output_consumer)
+
+    return io_time
+
+def bf_execute(program: str, in_data: list[int]=[], implementation='best') -> list[int]:
+    "Execute a brainfuck program with a given input. Returns data printed to stdout by the program"
+
+    return _bf_execute(program, in_data, _bf_get_interpreter(implementation))
+
+def bf_interactive_buffered_execute(program: str, implementation='best', input_prompt: bool=False) -> float:
+    "Execute a brainfuck program interactively. Input is sent to the program after user presses Enter. Returns time spent waiting for user input"
+
+    return _bf_interactive_buffered_execute(program, _bf_get_interpreter(implementation), input_prompt=input_prompt)
+
+def main():
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Interpret a brainfuck program')
+
+    parser.add_argument('program', nargs='?', default=None, help='Program to interpret')
+    parser.add_argument('--file', '-f', type=str, help='Take program from a file instead')
+    parser.add_argument('--implementation', type=str, default='best', choices=['best', 'basic', 'preprocessed'], help='Interpreter implementation name to use')
+
+    args = parser.parse_args()
+
+    if args.program and args.file:
+        print('Error: Multiple program input methods')
+        exit(1)
+
+    program = args.program
+    if args.file is not None:
+        try:
+            with open(args.file, mode='rt') as file:
+                program = file.read()
+                print(f'Read program from {args.file}. {len(program)} bytes read')
+        except Exception as e:
+            print(f'Error: failed to read file {args.file}: {e}')
+            exit(1)
+    elif program is None:
+        program = input("Input a brainfuck program: ")
+
     start_time = perf_counter()
     try:
-        output = bf_execute(program)
+        print('[Started program execution]')
+        io_time = bf_interactive_buffered_execute(program, args.implementation)
     except Exception as e:
-        print(f'Encountered error during execution: {e}')
-        output = []
+        io_time = 0
+        print(f'\n[Encountered error during execution: {e}. Terminating]')
 
-    print(f'Execution took {perf_counter() - start_time:0.2f}s')
-    text = ''.join(map(chr, output))
-    print(f'\nProgram output:\n{text}')
+    print(f'\n[Program terminated. Execution took {perf_counter() - start_time - io_time:0.2f}s. I/O time: {io_time:0.2f}s]')
+
+if __name__ == '__main__':
+    main()
